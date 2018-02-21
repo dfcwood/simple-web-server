4d5538bbeb745750985e78065bbb77a82fa721fe
Dan Calderwood <dcalderwood@icontrol.com>
2018-02-20 18:22:28 -0600

    First Commit

---
+++ src/main/java/simple_web_server/webserver/App.java
@@ -0,0 +1,13 @@
+package simple_web_server.webserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
